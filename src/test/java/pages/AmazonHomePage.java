6ded449675728a28d7ec80b1849e1f81dab101a0
helil-primetech <helil@primetechschool.com>
2021-06-10 20:44:31 -0400

    created indeed search tests with testng data provider

---
+++ src/test/java/pages/AmazonHomePage.java
@@ -0,0 +1,36 @@
+package pages;
+
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class AmazonHomePage {
+	
+	public AmazonHomePage() {
+		PageFactory.initElements(Driver.getDriver(), this);
+	}
+	
+	// if you work with element id, we just create an element abstract object.
+	@FindBy(id = "twotabsearchtextbox")
+	public WebElement searchBox;
+	
+	@FindBy(id = "nav-search-submit-button")
+	public WebElement searchButton;
+	
+	@FindBy(xpath = "//a[text()='Sign in securely']")
+	public WebElement signInButton;
+	
+	@FindBy (id = "searchDropdownBox")
+	public WebElement departmentsDropdown;
+	
+	@FindBy (xpath="//div[@data-component-type='s-search-result']//span[@class='a-price-whole']")
+	public List<WebElement> searchItemPrices;
+	
+	@FindBy (xpath="//span[@class='a-color-state a-text-bold']")
+	public WebElement searchResultText;
+
+	
+}
